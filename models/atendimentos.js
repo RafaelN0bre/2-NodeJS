f68d5ed1255b5e00fa459e33e0753709fda98c39
Rafael Nobre <rafaelmedeirosnobre2001@gmail.com>
2021-10-30 22:48:27 -0300

    Módulo 4

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,16 +1,52 @@
 const conexao = require('../infraestrutura/conexao')
 
+const moment = require('moment')
+const atendimentos = require('../controllers/atendimentos')
+
 class Atendimento {
-    adiciona(atendimento){
+    adiciona(atendimento, res){
+
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+        
+        //Validação de data, deve ser após a data de criação, de acordo com o nosso modelo de negócio. Retorna bool
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        
+        const clienteEhValido = atendimento.cliente.length >= 5
+
+        const validacoes = [
+            {
+                nome : 'data',
+                valido : dataEhValida,
+                mensagem : 'Data deve ser maior ou igual à data atual'
+            }, 
+            {
+                nome : 'cliente',
+                valido : clienteEhValido,
+                mensagem : 'Nome do cliente deve ter ao menos 5 caracteres'
+            }
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido )
+        // Se existem valores com campos não-válidos
+        const existemErros = erros.length
+
+        if(existemErros){
+            res.status(400).json(erros)
+        }else{
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+
         const sql = 'INSERT INTO atendimentos SET ?'
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if (erro){
-                console.log(erro)
+                res.status(400).json(erro)
             }else{
-                console.log(resultados)
+                res.status(201).json(atendimento)
             }
-        })
+        })    
+        }
+        
 
     }
 }
